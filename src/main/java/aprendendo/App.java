ab4df9a74f3b4926f337f775f313312cbbac79bc
joaopaulo <joaopaulosg17@gmail.com>
2023-09-14 10:21:15 -0300

    feat: Mudança na interpretação de valores numericos

--- src/main/java/aprendendo/App.java
+++ src/main/java/aprendendo/App.java
@@ -4,7 +4,7 @@ import aprendendo.utils.Utils;
 
 public class App {
     public static void main( String[] args ) throws Exception {
-        var tree = Utils.getFileContent("examples/add-simple.json");
+        var tree = Utils.getFileContent("examples/print.json");
         //System.out.println(tree.get("expression").get("kind").asText());
         System.out.println(Interpreter.eval(tree.get("expression")));;
     }
--- src/main/java/aprendendo/Interpreter.java
+++ src/main/java/aprendendo/Interpreter.java
@@ -10,14 +10,18 @@ public class Interpreter {
             case "Print": 
                 return eval(node.get("value"));
             case "Str":
-                return node.get("value");
+                return node.get("value").toString().replaceAll("\"","");
             case "Binary":
                 var lhs = eval(node.get("lhs"));
                 var rhs = eval(node.get("rhs"));
                 
                 switch(node.get("op").asText()) {
                     case "Add":
-                        return Integer.parseInt(lhs.toString()) + Integer.parseInt(rhs.toString());
+                        try {
+                            return Integer.parseInt(lhs.toString()) + Integer.parseInt(rhs.toString());
+                        }catch(Exception e) {
+                            return lhs.toString() + rhs.toString();
+                        }
                 }
                 break;
             case "Int":
