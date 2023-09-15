e1c4628be5d6bd61ea52fc5bfc2ec108439b25aa
joaopaulo <joaopaulosg17@gmail.com>
2023-09-15 13:50:05 -0300

    feat: Adicionado suporte para funções (implementação basica)

--- src/main/java/aprendendo/Interpreter.java
+++ src/main/java/aprendendo/Interpreter.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
-
+@SuppressWarnings("unchecked")
 public class Interpreter {
     private static final Map<String,Object> variaveis = new HashMap<>();
-
+    private static final Map<String,Object> params = new HashMap<>();
+    private static JsonNode jsonNode = null;
     public static Object eval(JsonNode node){
         switch(node.get("kind").asText()) {
             case "Print": 
@@ -42,14 +43,32 @@ public class Interpreter {
                 return node.get("value");
 
             case "Let":
-                var name = node.get("name").get("text");
+                var name = node.get("name").get("text").asText();
                 var variavel = eval(node.get("value"));
                 variaveis.put(name.toString(),variavel);
                 return eval(node.get("next"));
 
             case "Var":
-                return variaveis.get(node.get("text").toString());
-            }
+                return variaveis.get(node.get("text").asText());
+            case "Function":
+                node.get("parameters").forEach(((item) -> {
+                    params.put(item.get("text").asText(),null);
+                }));
+                jsonNode = node.get("value");
+                return params;
+            case "Call":
+                Map<String,Object> param =(Map<String,Object>) eval(node.get("callee"));
+                
+                int i = 0;
+                for(var value : param.keySet()) {
+                    param.put(value,eval(node.get("arguments").get(i)));
+                    i++;
+                }
+                for(var value : param.keySet()) {
+                    variaveis.put(value,param.get(value));
+                }
+                return eval(jsonNode);       
+        }
 
         return null;
     }
