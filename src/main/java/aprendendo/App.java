1c77d8b3b715c7374097b72b62f6b78ab3e832b9
JoãoPaulo <joaopaulosg17@gmail.com>
2023-09-24 11:26:07 -0300

    mudança no tipo de arquivo

--- src/main/java/aprendendo/App.java
+++ src/main/java/aprendendo/App.java
@@ -1,21 +1,14 @@
 package aprendendo;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import aprendendo.interpreter.ExecuteInterpreter;
-import aprendendo.utils.FileUtils;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        if(args.length > 0 && args.length == 1) {
-            ExecuteInterpreter execute = new ExecuteInterpreter(Paths.get(args[0]));
-            switch(FileUtils.fileType(execute.getFilePath())) {
-                case "json" -> execute.executeJson();
-                case "rinha" -> execute.executeOther();
-            }
-        }else{
-            throw new RuntimeException("Args invalidos");
-        }
-        
+        Path path = Paths.get("/var/rinha/source.rinha.json");
+        ExecuteInterpreter execute = new ExecuteInterpreter(path);
+        execute.executeJson();    
     }
 }
